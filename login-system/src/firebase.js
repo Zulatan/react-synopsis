3e989d978e4644f7b2d6435d3dff286ec5117d84
Mikkel Lau <mikkellau98@hotmail.com>
2024-06-18 12:37:01 +0200

    changes to env file

--- login-system/src/firebase.js
+++ login-system/src/firebase.js
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import{ getAuth } from "firebase/auth";
-import dotenv from 'dotenv';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
 // Load environment variables from .env file
-dotenv.config();
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
